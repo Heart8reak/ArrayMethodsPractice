6a67f3de51dc9fc04277dc564e6b82e3246cce51
Carlos Baez <carlosbaez@Green-Planet.local>
2019-10-14 23:28:22 -0400

    Initial commit with Array/Object Practice

---
+++ app.js
@@ -0,0 +1,34 @@
+let arrayPrac = 'This better be working!'
+
+console.log(arrayPrac)
+
+
+const greatfulList = [
+    'I am grateful for my children',
+    'I am grateful for my health'
+]
+
+const greatSchoolList = [
+    'I am proud that I am actually learning Javascript',
+    'I am proud that I am actually learning how to understand code',
+]
+
+console.log(greatfulList.pop())
+console.log(greatfulList)
+
+console.log(greatfulList.push('I am grateful for my XBOX', 'I am grateful for my mother!'))
+
+console.log(greatfulList)
+console.log(greatfulList.shift())
+
+console.log(greatfulList)
+console.log(greatfulList.unshift('I am grateful for my experience in the US Marines'))
+console.log(greatfulList)
+
+console.log(greatSchoolList.pop())
+console.log(greatSchoolList)
+console.log(greatSchoolList.push('I am eager to learn Java', 'I am eager to become a Full Stack Engineer'))
+console.log(greatSchoolList.shift())
+console.log(greatSchoolList)
+console.log(greatSchoolList.unshift('I am going to start an Software Agency!'))
+console.log(greatSchoolList)
